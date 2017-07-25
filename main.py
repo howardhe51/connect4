bf91780e1ba5df93c9ffebfd6f9d3140303f4255
howardhe51 <howardhe51@gmail.com>
2017-07-25 08:34:53 -0700

    Added some comments for outlining

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+import json
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -16,6 +17,10 @@ class User(ndb.Model):
     username = ndb.StringProperty()
     win = ndb.IntegerProperty()
     lose = ndb.IntegerProperty()
+class Game(ndb.Model):
+    board = ndb.JsonProperty()
+    # Might want to have a `winner` UserProperty
+    # Keep track of user1 and user2
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -31,6 +36,7 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/main.html')
         self.response.write(template.render(template_vars))
 
+# This handler will take in the column that the user selected
 class SelectionHandler(webapp2.RequestHandler):
     def get(self):
 
@@ -42,6 +48,20 @@ class SelectionHandler(webapp2.RequestHandler):
 
 
 
+class NewGameHandler(webapp2.RequestHandler):
+    def get(self):
+        board = [ [0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0]]
+        board1 = json.dumps(board)
+        new_game = Game(board = board1)
+        # 1. Create a new game when users go to /newgame
+        # 2. Add handler (like /game/:id) to get an existing game, based on the Game id
+        # 3. When the user clicks on a column (JS), run the /selectionHandler to update the game
+        # 4. The Game should keep track of whose turn it is (Python), and auto-refresh the UI if it's no that person's turn (JS)
 
 
 
@@ -67,9 +87,11 @@ class SelectionHandler(webapp2.RequestHandler):
 
 
 
-    
+
 class MoveHandler(webapp2.RequestHandler):
     def get(self):
+    def post(self):
+
 
 
 

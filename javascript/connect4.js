eff2939c68df16b778391098a3831b1ae3e443c7
howardhe51 <howardhe51@gmail.com>
2017-07-25 08:41:34 -0700

    test1

--- javascript/connect4.js
+++ javascript/connect4.js
@@ -8,7 +8,10 @@ var board = [ [0,0,0,0,0,0,0],
 function drawBoard() {
   for(i = 0; i < 6; i++) {
     for( j = 0; j < 7; j++) {
-
     }
   }
 }
+
+// Add a click event handler.
+// Use $.post to "call" the /selectionHandler with the column that the user clicked on
+// Use the result that the /selecdtionHandler gives back to update the board and see if there's a winner

b5df7f56461b63d7a22b156f675b57d72214df18
howardhe51 <howardhe51@gmail.com>
2017-07-25 19:19:32 -0700

    connect four board dropping and drawing works, still need work on checking the win

--- javascript/connect4.js
+++ javascript/connect4.js
@@ -3,7 +3,6 @@
 function clickColumn() {
   var button = $(this);
   var col = Number($(this).attr('id'));
-  var urlsafeKey = $(button).val();
   $.post('/column', {'column': col}, function(game) {
     // Update the number in the "like" element.
     console.log("Game", game)
@@ -23,10 +22,10 @@ function clearBoard() {
 function drawBoard(game) {
   for(i = 0; i< 7; i++) {
     for(j = 0; j<6; j++) {
-      if(game[j][i]==1){
+      if(game[i][j]==1){
         $("#square" + i + j).addClass('p1')
       }
-      else if(game[j][i]==2){
+      else if(game[i][j]==2){
         $("#square" + i + j).addClass('p2')
       }
     }

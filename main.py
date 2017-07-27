924b8ce953a4e7208af20819194a9d3bc6654ffb
Hibo Osman <hibozahara@gmail.com>
2017-07-26 21:25:49 -0700

    css changes

--- main.py
+++ main.py
@@ -82,9 +82,10 @@ class ProfileHandler(webapp2.RequestHandler):
         self.response.write(template.render(template_vars))
 
     def post(self):
-        image = self.request.get('filename')
+        image = self.request.get('img_link')
         # image is a "unicode" type, but we want it to be a string
-        image = str(image)
+        #image = str(image)
+        print "the link is ", image
         current_user = users.get_current_user()
         user_email = current_user.email()
         user = User(email=user_email, image = image)
